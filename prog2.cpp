ac90bf787decb373d369be242435c61e421eb897
Sieh <1960681@linux103.mcs.sdsmt.edu>
2015-10-30 20:48:57 -0600

    Switching to VS for testing

--- prog2.cpp
+++ prog2.cpp
@@ -5,7 +5,7 @@
 #include <limits.h>
 #include <math.h>
 #include <algorithm>
-#include <stack>
+#include <float.h>
 
 using namespace std;
 
@@ -21,6 +21,9 @@ bool isConvex(vector<point> pVector);
 double direction(point a, point b, point c);
 double cross(point a, point b);
 double area(vector<point> pVector);
+bool intersect(point p1, point p2, point p3, point p4);
+int bisection(vector<point> pVector);
+bool on(point a, point b, point c);
 
 int main(int argc, char * argv[])
 {
@@ -67,6 +70,8 @@ int main(int argc, char * argv[])
 		}
 
 		cout << "Area: " << area(pVector) << endl;
+
+		bisection(pVector);
 
 		pVector.clear();
 		cout << "CLEAR!" << endl;
@@ -116,29 +121,99 @@ double cross(point a, point b)
 	return a.x * b.y - a.y * b.x;
 }
 
-int bisection(vector<point> pVector, float lArea, float rArea)
+int bisection(vector<point> pVector)
 {
+	int n = pVector.size();
 	double cut = 0;
 	double maxX = 0;
 	double minX = FLT_MAX;
 	double midX = 0;
+	double maxY = 0;
+	double minY = FLT_MAX;
+	point botPoint;
+	point topPoint;	
 
-	for(int i = 0; i < pVector.size; i++)
+	for(int i = 0; i < n; i++)
 	{
-		minX = min(minX, pVector[i]);
-		maxX = max(maxX, pVector[i]);
+		minX = min(minX, pVector[i].x);
+		maxX = max(maxX, pVector[i].x);
+		minY = min(minY, pVector[i].y);
+		maxY = max(maxY, pVector[i].y);	
 	}
 
 	midX = (maxX - minX)/2;
-
-	if(lArea == rArea)
-		return cut;
-	if(lArea > rArea)
-
-	if(lArea < rArea) 
-
 	
+	topPoint.x = midX;
+	topPoint.y = maxY + 1;
+	botPoint.x = midX;
+	botPoint.y = minY;	
+
+	cout << "topPoint.x: " << topPoint.x << endl;
+	cout << "topPoint.y: " << topPoint.y << endl;
+	cout << "botPoint.x: " << botPoint.x << endl;
+	cout << "botPoint.y: " << botPoint.y << endl;
+
+	for(int i = 0; i < n; i++)
+	{
+		bool test;	
+		cout << "pVector[i].x: " << pVector[i].x << endl;
+		cout << "pVector[i].y: " << pVector[i].y << endl;
+		cout << "pVector[i+1].x: " << pVector[(i+1) % n].x << endl;
+		cout << "PVector[i+1].y: " << pVector[(i+1) % n].y << endl;
+		test = intersect(botPoint, topPoint, pVector[i], pVector[i+1]);
+		if(test == true)
+			cout << "Intersect" << endl;
+		else
+			cout << "No Intersect" << endl;
+	}
 }
+
+bool intersect(point p1, point p2, point p3, point p4)
+{
+	double d1;
+	double d2;
+	double d3;
+	double d4;
+
+	d1 = direction(p3, p4, p1);
+	d2 = direction(p3, p4, p2);
+	d3 = direction(p1, p2, p3);
+	d4 = direction(p1, p2, p4);
+
+	cout << "d1: " << d1 << endl;
+	cout << "d2: " << d2 << endl;
+	cout << "d3: " << d3 << endl;
+	cout << "d4: " << d4 << endl;
+
+	if ((((d1 > 0) && (d2 < 0)) || ((d1 < 0) && (d2 > 0))) &&
+	(((d3 > 0) && (d4 < 0)) || ((d3 < 0) && (d4 > 0))))
+		return true;
+
+	if ((d1 == 0) && on(p3, p4, p1))
+		return true;
+
+	if ((d2 == 0) && on(p3, p4, p2))
+		return true;
+
+	if ((d3 == 0) && on(p1, p2, p3))
+		return true;
+
+	if ((d4 == 0) && on(p1, p2, p4))
+		return true;
+
+	return false;
+}
+
+bool on(point a, point b, point c)
+{
+	if (((min(a.x, b.x) <= c.x) && (c.x <= max(a.x, b.x))) &&
+	((min(a.y, b.y) <= c.y) && (c.y <= max(a.y, b.y))))
+		return true;
+
+	return false;
+}
+
+
 
 //Corwin
 //edited by me
